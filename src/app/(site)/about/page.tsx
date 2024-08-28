3bd1e55df9e2122a7977cd9d411f08fc3c6d2fe5
Viktor Mihalkov <viktor_mihalkov@protonmail.com>
2024-08-28 19:23:34 +0300

    page/about

--- src/app/(site)/about/page.tsx
+++ src/app/(site)/about/page.tsx
@@ -4,15 +4,16 @@ import Team from "@/components/Team";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title:
-    "About Us | Play SaaS Starter Kit and Boilerplate for Next.js",
-  description: "This is About page description",
+  title: "За Нас | Car Deal - Вашият Партньор за Внос на Автомобили от Германия до България",
+  description: 
+  "Научете повече за Car Deal, вашият доверен партньор за внос на автомобили от Германия до България. Открийте как можем да ви помогнем да намерите и доставите вашата мечтана кола.",
 };
+
 
 const AboutPage = () => {
   return (
     <main>
-      <Breadcrumb pageName="About Us Page" />
+      <Breadcrumb pageName="За Нас" />
       <About />
       <Team />
     </main>
