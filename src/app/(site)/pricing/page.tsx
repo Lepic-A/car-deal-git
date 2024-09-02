404301e99a46d93754d30285cf0baa7e1c025718
Viktor Mihalkov <viktor_mihalkov@protonmail.com>
2024-09-02 06:37:27 +0300

    translation pricing

--- src/app/(site)/pricing/page.tsx
+++ src/app/(site)/pricing/page.tsx
@@ -4,15 +4,14 @@ import Pricing from "@/components/Pricing";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title:
-    "Pricing Page | Play SaaS Starter Kit and Boilerplate for Next.js",
-  description: "This is pricing page description",
+  title: "Цени | Car Deal - Внос на Автомобили от Германия до България",
+  description: "Вижте нашите цени за внос на автомобили от Германия до България. Ние предлагаме конкурентни и прозрачни цени за качествени услуги.",
 };
 
 const PricingPage = () => {
   return (
     <>
-      <Breadcrumb pageName="Pricing Page" />
+      <Breadcrumb pageName="Цени" />
       <Pricing />
       <Faq />
     </>
